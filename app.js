3af0958019a11e2b4caca91d4ae47f927fe800fb
ALYSSA EVANS <ALYSSA.M.EVANS@GMAIL.COM>
2017-07-03 09:51:28 -0700

    rendering html with small_test.json, working on a singly nested json structure

--- app.js
+++ app.js
@@ -1,20 +1,31 @@
 (function () {
-
-  $('.formy').submit(e => {
-    e.preventDefault()
-    var $file = $('#jsonFile')[0]['files']
+  $('.formy').change(() => {
+    let $file = $('#jsonFile')[0]['files']
     let file = $file[0]
-    console.log('filey ', file);
+    let parseObj
 
-    var fr = new FileReader()
-    fr.readAsText(file)
-    fr.onload = (function(theFile) {
-      return function(e) {
-       let result = e.target.result
-       let parseObj = JSON.parse(result)
-       console.log(parseObj);
-      }
+    let fr = new FileReader()
+    let readed = fr.readAsText(file)
+    console.log(readed);
+    fr.onload = (function () {
+      return readAndParse
     })()
-  })
 
+    function readAndParse(e) {
+     let result = e.target.result
+     parseObj = JSON.parse(result)
+     console.log(parseObj)
+     jsonRecursion(parseObj)
+
+    }
+
+    function jsonRecursion(obj) {
+      if (obj.content.tag) {
+        jsonRecursion(obj.content)
+      }
+      $('.json').append(`<${obj.tag}>${obj.content}</${obj.tag}>`)
+    console.log(obj.content);
+    }
+
+  })
 })()
