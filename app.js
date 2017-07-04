281ef7bee16140da98ed782c46f3e68693249af2
ALYSSA EVANS <ALYSSA.M.EVANS@GMAIL.COM>
2017-07-04 09:14:51 -0700

    file not being replaced by new files; still looping twice, looking to refactor and append only one set of html

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-(function () {
   $('.file-upload').change(() => {
     let $file = $('#json-file')[0]['files']
     let file = $file[0]
@@ -12,23 +11,24 @@
     function readAndParse(e) {
      let result = e.target.result
      let parseObj = JSON.parse(result)
+     $('.file-render').replaceWith(parseObj)
      return loop(parseObj, '.json-render')
     }
 
-    function loop(jsonFile, parent) {
-      jsonFile.forEach(obj => {
+    function loop(jsonObj, parent) {
+      jsonObj.forEach(obj => {
         console.log('fe obj ', obj);
         if (obj.content.tag) {
-          jsonRecursion(obj, parent)
+          return jsonRecursion(obj, parent)
         } else if (Array.isArray(obj.content)) {
           $(parent).append(`<${obj.tag}>`)
-          loop(obj.content, obj.tag)
+          return loop(obj.content, obj.tag)
         } else {
           return $(parent).append(`<${obj.tag}>${obj.content}</${obj.tag}>`)
         }
       })
     }
-
+//either its appending twice at the beginning or end.
     function jsonRecursion(obj, parent) {
       console.log('justObj ', obj)
       if (obj.content.tag) {
@@ -36,10 +36,9 @@
         return $(`${obj.tag}`).append(jsonRecursion(obj.content, obj.tag))
       } else if (Array.isArray(obj.content)) {
         $(parent).append(`<${obj.tag}>`)
-        loop(obj.content, obj.tag)
+        return loop(obj.content, obj.tag)
       }
       return $(parent).append(`<${obj.tag}>${obj.content}</${obj.tag}>`)
     }
 
   })
-})()
