a2231d85054813a294daa9d99e9912ba959ca49c
ALYSSA EVANS <ALYSSA.M.EVANS@GMAIL.COM>
2017-07-03 18:41:59 -0700

    most nested content being rendered; some tags being rendered as separate objects

--- app.js
+++ app.js
@@ -2,7 +2,6 @@
   $('.file-upload').change(() => {
     let $file = $('#json-file')[0]['files']
     let file = $file[0]
-    let parseObj
 
     let fr = new FileReader()
     fr.readAsText(file)
@@ -12,27 +11,36 @@
 
     function readAndParse(e) {
      let result = e.target.result
-     parseObj = JSON.parse(result)
-     return jsonRecursion(parseObj)
+     let parseObj = JSON.parse(result)
+     return loop(parseObj)
+    }
+
+    function loop(jsonFile) {
+      jsonFile.forEach(obj => {
+        console.log('fe obj ', obj);
+        if (obj.content.tag) {
+          jsonRecursion(obj)
+        } else if (Array.isArray(obj)) {
+          loop(obj)
+        } else {
+          append(`<${obj.tag}>${obj.content}</${obj.tag}>`)
+        }
+      })
     }
 
     function jsonRecursion(obj) {
-      if (Array.isArray(obj)){
-        console.log('is array');
-        for (var i = 0; i < parseObj.length; i++) {
-          console.log(obj[i]);
-          append(obj[i].tag, obj[i].content)
+      console.log('justObj ', obj)
 
-          if (obj[i].content.tag) {
-            jsonRecursion(obj[i].content)
-            append(obj[i].content.tag, obj[i].content.content)
+          if (obj.content.tag) {
+            append(`<${obj.content.tag}>`)
+            jsonRecursion(obj.content)
+            $(`${obj.content.tag}`).append(`${obj.content.content}`)
           }
-        }
-      }
+            // append(`</${obj.tag}>`)
     }
 
-    function append (tag, content) {
-      return $('.json-render').append(`<${tag}>${content}</${tag}>`)
+    function append (html) {
+      $('.json-render').append(html)
     }
 
   })
